b31e0830e67303858518ad77dd1d3236e3c50dbd
ales.pokora <ales.pokora@dataddo.com>
2025-10-08 11:15:18 +0200

    updated config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
+  routeRules: {
+    "/": { ssr: true },
+    "/**": { ssr: true },
+  },
   modules: [
     "@nuxt/content",
     "@nuxt/eslint",
